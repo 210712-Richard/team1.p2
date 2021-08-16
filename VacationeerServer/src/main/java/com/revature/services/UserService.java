bc5a65226b50995c634576b1bbb473a8feeb9a15
mcinerneym <mcinerney.michael.t@gmail.com>
2021-08-16 15:09:12 -0700

    Added the method stubs for login, register, and logout

--- VacationeerServer/src/main/java/com/revature/services/UserService.java
+++ VacationeerServer/src/main/java/com/revature/services/UserService.java
@@ -1,5 +1,15 @@
 package com.revature.services;
 
-public interface UserService {
+import java.time.LocalDate;
 
+import com.revature.beans.User;
+import com.revature.beans.UserType;
+
+import reactor.core.publisher.Mono;
+
+public interface UserService {
+	public Mono<User> login(String username, String password);
+
+	public Mono<User> register(String username, String password, String email, String firstName, String lastName,
+			LocalDate birthday, UserType type);
 }
