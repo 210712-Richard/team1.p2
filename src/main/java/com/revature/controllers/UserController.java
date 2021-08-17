9de0b4f2127276c7fa66f98ff269d14b238d1ac9
mcinerneym <mcinerney.michael.t@gmail.com>
2021-08-17 11:53:23 -0700

    Removed unused imports in usercontroller

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,11 +1,6 @@
 package com.revature.controllers;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.WebSession;
 
 import com.revature.beans.User;
