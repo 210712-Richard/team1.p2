5d953f7eb2c2fe7f74fd0a26c1f93250fc0b3d39
KyleBricker <kylejbricker@yahoo.com>
2021-08-25 12:52:59 -0700

    last smell

--- src/main/java/com/revature/controllers/ActivityControllerImpl.java
+++ src/main/java/com/revature/controllers/ActivityControllerImpl.java
@@ -1,7 +1,5 @@
 package com.revature.controllers;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
