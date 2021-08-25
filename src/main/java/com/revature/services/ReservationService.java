fc84530efaab9bba781982d8bb0570ebc7f6445b
steven-gsx <87335033+steven-gsx@users.noreply.github.com>
2021-08-25 14:37:02 -0700

    Update ReservationService.java
    Merge conflict edits for SC

--- src/main/java/com/revature/services/ReservationService.java
+++ src/main/java/com/revature/services/ReservationService.java
@@ -16,6 +16,7 @@ public interface ReservationService {
 	public Mono<Reservation> reserveHotel(Hotel hotel, Vacation vacation);
 	public Mono<Reservation> reserveFlight(Flight flight, Vacation vacation);
 	public Mono<Reservation> reserveCar(Car car, Vacation vacation)	;
+	public Mono<Reservation> getReservation(UUID resId);
 	public Mono<Reservation> findReservation(String resId);
 	public Mono<Reservation> updateReservation(String resId, String status);
 	public Flux<Reservation> getReservations(String username, String vacId);
